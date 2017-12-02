7fef91e1173b4cab8ac02fc72a8d4cc149054353
Kevin Berry <kpberry11@gmail.com>
2017-12-02 15:09:16 -0500

    Adds LS1 and LS2 tests

---
+++ run_LS1_tests.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+for algo in LS1;
+do
+    mkdir ${algo}
+    for filename in ./data/Data/*.graph; do
+        for ((i=0;i<4;i+=1))
+        do
+            echo ${filename}
+            echo ${algo}
+            python3 main.py -inst ${filename} -alg ${algo} -seed $i -time 1000 &
+        done
+        sleep 1030
+        mv *.trace *.sol ${algo}/
+    done
+    mkdir ${algo}
+    for filename in ./data/Data/*.graph; do
+        for ((i=4;i<8;i+=1))
+        do
+            echo ${filename}
+            echo ${algo}
+            python3 main.py -inst ${filename} -alg ${algo} -seed $i -time 1000 &
+        done
+        sleep 1030
+        mv *.trace *.sol ${algo}/
+    done
+    mkdir ${algo}
+    for filename in ./data/Data/*.graph; do
+        for ((i=8;i<12;i+=1))
+        do
+            echo ${filename}
+            echo ${algo}
+            python3 main.py -inst ${filename} -alg ${algo} -seed $i -time 1000 &
+        done
+        sleep 1030
+        mv *.trace *.sol ${algo}/
+    done
+done
